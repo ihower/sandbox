ed4d9fd03e6ed2b32e653acb11c20b71c4259cae
Wen-Tien Chang <ihower@gmail.com>
2012-07-20 21:58:15 +0800

    Init a Rails project to git demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandbox::Application.config.secret_token = '96018fa584d2c89c13478acafdbcf6709fd341e6ef3e958a8b30c85318eb3003c52a4447462c75105d634b8fe3dced2a1dd1c9b7903db7c9ffe97693e6dab430'
