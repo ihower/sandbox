51fa3822b94ee902e54d2d85a505b7758b554aee
Wen-Tien Chang <ihower@gmail.com>
2012-07-20 22:14:57 +0800

    Add user scaffold

---
+++ spec/views/users/edit.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "users/edit" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :name => "MyString",
+      :bio => "MyText"
+    ))
+  end
+
+  it "renders the edit user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => users_path(@user), :method => "post" do
+      assert_select "input#user_name", :name => "user[name]"
+      assert_select "textarea#user_bio", :name => "user[bio]"
+    end
+  end
+end
